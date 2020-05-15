b838536a9de86537129ba713fb2d54bf2299f4dc
viniciuswoloszyn <woloszyn@tu-berlin.de>
2020-05-15 16:39:03 +0200

    adding description

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# Scientific_ner
+# Automatic Interpretation of Scientific Papers.
+
+There has been an exponential increase in the volume of scientific publications in the past decades, making the organization of the scientific information a strategical task for identifying new technologies and their relationships with what existed before.
+
+
+## 
+
+
+
+## Methodology
 
 Sample abstracts from a semantic scholar gzip dataset files added in 'semanticScholarParser' folder
 
--- code/data_acquisition/json_to_doccano.py
+++ code/data_acquisition/json_to_doccano.py
@@ -1,32 +1,26 @@
 import pandas as pd
 import json
 import numpy as np
-N=200
-input=["../abstracts.json"]
-output="../output.json"
-
+N=400
+input=["../../data/raw/abstracts_800.json"]
+output="../../data/raw/doccano_400.jsonl"
 
 def get_json(file_name):
-    print (file_name)
     data = []
     for file in file_name:
         with open(file) as f:
             for line in f:
-                #print (line)
                 row=json.loads(line)
                 data.append(row)
     return data
 
-data=[]
-for l in get_json(input)[-N:]:
-    data.append({"text":l["text"].encode("utf-8"),"labels":[]} )
+dataset=get_json(input)
+np.random.shuffle(dataset)
+data=[{"text":l["paperAbstract"].replace("\n"," "),"labels":[]} for l in dataset[-N:]  if l["paperAbstract"].replace("\n","")!=""]
 
-print (data)
-#pd.DataFrame(data).reset_index().to_json(output)
-#np.savetxt(output, data)
 with open(output, 'a') as f:
     for d in data:
-        f.write( json.dumps(d)+"\n")
+        f.write(json.dumps(d)+"\n")
 
 
 
